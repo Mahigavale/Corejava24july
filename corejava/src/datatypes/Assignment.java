4f7a1718f34213d926eb174ab882d82052ca1ad3
Mahigavale <maheshgavale07@gmail.com>
2024-07-29 11:23:30 +0530

    Assignment operator

---
+++ corejava/src/datatypes/Assignment.java
@@ -0,0 +1,39 @@
+package datatypes;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// assignment opt.
+		//logical
+		
+		int var=123;
+		System.out.println(var+=5);
+		//System.out.println(var+5);
+		float f=4.0f;
+		
+		System.out.println(f+=1.0);
+		
+		int x=1;
+		
+		System.out.println(x+=1); //x=x+1;
+		
+		System.out.println(x);
+		
+		
+		
+		// var+5= => var=var+5;
+		
+		
+		
+		int demo=45;
+		System.out.println(demo-=5); //demo=demo-5;
+		System.out.println(demo*=2); //demo=demo*2;
+		System.out.println(demo/=3);  //demo=demo/3;
+		System.out.println(demo%=13); //demo=demo%13;
+		
+		
+
+	}
+
+}
