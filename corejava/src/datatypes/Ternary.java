0a274e6306f1dc786b43d5e4fc8e3592d13e3ab1
Mahigavale <maheshgavale07@gmail.com>
2024-07-31 13:13:35 +0530

    Ternary

---
+++ corejava/src/datatypes/Ternary.java
@@ -0,0 +1,34 @@
+package datatypes;
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		
+		//12+10=>binary 
+		//13+1+12+12+67+78=>
+		//12+A=>int +number
+		//ter=> three operands ! =>indep.
+		
+		
+		int num=126; //126
+		
+//		if(num<125)
+//		{
+//			System.out.println("number is less than 125!");
+//		}
+//		else
+//		{
+//			System.out.println("number is greater than 125 !");
+//		}
+    
+		System.out.println((num>125)? true:false);
+		System.out.println((num>125)? "yes":false);
+		System.out.println((num<125)? 1:"no");
+		
+		int age=18;
+		System.out.println((age>=18)? "yes":"no");
+	}
+
+}
