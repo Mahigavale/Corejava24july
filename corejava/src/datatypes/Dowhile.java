534cad322b38202c806c8590ffbbeeb42497cc55
Mahigavale <maheshgavale07@gmail.com>
2024-07-31 13:14:33 +0530

    do while

---
+++ corejava/src/datatypes/Dowhile.java
@@ -0,0 +1,30 @@
+package datatypes;
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int num=10;
+//		int multi=1;
+//		
+//		do {
+//			System.out.println(num*multi);
+//			multi++;
+//		}while(multi<=10);
+
+		
+		int num3=10;
+		
+		 //<,<=  (10<10)=>false
+		do//=>do this  at least once gets executed !
+		{
+			System.out.println("Print ");
+			num3++;
+		}while(num3 <10); //10<10=>false
+		System.out.println("**********THE END********");
+			 
+			 
+	}
+
+}
